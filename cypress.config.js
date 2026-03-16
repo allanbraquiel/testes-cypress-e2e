706b740ecc365719b1b9847d48c941db1123a402
Allan Braquiel <alnbraquiel@gmail.com>
2026-03-16 15:34:35 -0300

    Ajustes no teste de login

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    //baseUrl: "http://localhost",
-    baseUrl: "http://127.0.0.1:8000",
+    baseUrl: "http://localhost",
+    //baseUrl: "http://127.0.0.1:8000",
 
     viewportHeight: 900,
     viewportWidth: 1440,
